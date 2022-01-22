0ff8d4e4f675f5284a415e5ff144a5b2940a6905
Liza <lizzeykudi>
2022-01-22 17:56:55 +0300

    work on hw 12 spring security

---
+++ hw12_authorization/src/main/java/ru/otus/spring/domain/user/User.java
@@ -0,0 +1,33 @@
+package ru.otus.spring.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+
+
+
+}
