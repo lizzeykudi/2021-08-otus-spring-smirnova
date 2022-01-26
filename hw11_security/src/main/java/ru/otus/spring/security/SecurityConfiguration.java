58d6597cf98fa8f200414e91ea5012d2b14147db
lizzeykudi <lizzeykudi@gmail.com>
2022-01-26 19:01:35 +0300

    work on hw11

--- hw11_security/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
+++ hw11_security/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
@@ -2,11 +2,8 @@ package ru.otus.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -29,6 +26,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
             throws Exception {
 
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/css/**").permitAll() //Adding this line solved it
                 .antMatchers("/login").anonymous()
